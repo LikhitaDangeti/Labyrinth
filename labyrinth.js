d857ac5484ce1a7ddfae3a2739592b2a5de4e660
Likhita Dangeti <122101271+LikhitaDangeti@users.noreply.github.com>
2024-11-22 17:35:08 +0530

    Update labyrinth.js

--- labyrinth.js
+++ labyrinth.js
@@ -1,1 +1,173 @@
-//Labyrinth code.
+const p1Name = '😁';
+const p2Name = '😎';
+const p3Name = '😴';
+const p4Name = '😳';
+
+function getHeading() {
+  return '┏━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┓';
+}
+
+function getFooting() {
+  return '┗━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┛';
+}
+
+function getRowFooting() {
+  return '┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫';
+}
+
+function getCharsInCell(p1Pos,cellNumber, bombBoxNumber) { // For placing object in a cell
+  if (cellNumber === bombBoxNumber) {
+    return placeObject('💥');
+  }
+
+  if (p1Pos === cellNumber) {
+    return placeObject(p1Name);
+  }
+  return placeObject('⬜');
+  // return placeObject(cellNumber);
+}
+
+function placeObject(object) {
+  return ' ' + object + ' ┃';
+}
+
+function createRow(p1Pos,rowStartNumber, bombBoxNumber) { // For Single Row
+  let rowString = '';
+
+  for (let boxNumber = rowStartNumber; boxNumber > rowStartNumber - 10; boxNumber--) {
+    rowString += getCharsInCell(p1Pos,boxNumber, bombBoxNumber);
+  }
+
+  return '┃' + rowString;
+}
+
+function createGrids(p1Pos,bombBox) { // For creating Grid
+  let grid = getHeading() + '\n';
+
+  for (let noOfRows = 10; noOfRows > 0; noOfRows--) {
+    grid += createRow(p1Pos,noOfRows * 10, bombBox) + '\n';
+
+    if (noOfRows !== 1) {
+      grid += getRowFooting() + '\n';
+    }
+  }
+
+  return grid + getFooting();
+}
+
+function updatePlayersPosition(position) {
+  return createGrids(position);
+}
+
+
+function getRandomNoInRange(min, max) { // 90, 100 (For Ending position to be random)
+  let randomNumber = 0;
+  while (!(randomNumber <= max && randomNumber >= min)) {
+    randomNumber = Math.ceil(Math.random() * 100);
+  }
+
+  return randomNumber;
+}
+
+function getBombPosition() {
+  return Math.ceil(Math.random() * 100);
+}
+
+function isCorrectBombPosition(bombPosition) { // to check if 
+  let isCorrectPosition = bombPosition - 11 !== '🧨';
+  isCorrectPosition = isCorrectPosition && (bombPosition - 10) !== '🧨';
+  isCorrectPosition = isCorrectPosition && (bombPosition - 9) !== '🧨';
+
+  isCorrectPosition = isCorrectPosition && (bombPosition - 1) !== '🧨';
+  isCorrectPosition = isCorrectPosition && (bombPosition + 1) !== '🧨';
+
+  isCorrectPosition = isCorrectPosition && (bombPosition + 9) !== '🧨';
+  isCorrectPosition = isCorrectPosition && (bombPosition + 10) !== '🧨';
+  isCorrectPosition = isCorrectPosition && (bombPosition + 11) !== '🧨';
+
+  return isCorrectPosition;
+}
+
+function generateBombs() {
+  let totalNoOfBobms = 20;
+  let bombPosition = '';
+
+  while (totalNoOfBobms > 0) {
+    bombPosition += getBombPosition() + ' ';
+    if (isCorrectBombPosition(bombPosition)) {
+      totalNoOfBobms -= 1;
+    }
+  }
+
+  return bombPosition;
+}
+
+function didPlayerWin(position, endPosition) {
+  return position === endPosition;
+}
+
+function getEndPosition() {
+  return getRandomNoInRange(90, 100);
+}
+
+function getStartPosition() {
+  const position = +prompt("Enter your starting position(1-10): ", "0");
+
+  if (position > 10) {
+    console.log("Starting position can only be between 1 and 10");
+    console.log("Enter the valid position!!!");
+
+    return getStartPosition();
+  }
+
+  return position;
+}
+
+function isBombEncountered(position, bombPositions) {
+  let currentBombPos = '';
+
+  for (let index = 0; index < bombPositions.length; index++) {
+    currentBombPos += bombPositions[index];
+
+    if (bombPositions[index] === ' ') {
+      if (+currentBombPos === position) {
+        return true;
+      }
+      currentBombPos = '';
+    }
+
+  }
+
+  return false;
+}
+
+function startGame() {
+  const bombPositions = generateBombs();
+  console.log(bombPositions);
+
+  const endPosition = getEndPosition();
+  // console.log(endPosition);
+
+  let startPosition = 0;
+  console.log(createGrids());
+
+  while (!didPlayerWin(startPosition, endPosition)) {
+    if (startPosition < 1) {
+      startPosition = getStartPosition();
+    }
+    console.clear();
+
+    if (isBombEncountered(startPosition, bombPositions)) {
+      console.clear();
+      startPosition = 0;
+    }
+
+    console.log(updatePlayersPosition(startPosition));
+    break;
+  }
+  // console.log(endPosition);
+}
+
+startGame();
+
+// console.log(updatePlayersPosition(10));//Labyrinth code.
